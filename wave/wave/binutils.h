e8b68cbf0506f8f4b09681340ed2f7032ae35fed
hendrixjl <hendrix.john64@gmail.com>
2013-06-28 13:41:48 -0500

    add buffered read function

--- wave/wave/binutils.h
+++ wave/wave/binutils.h
@@ -17,6 +17,14 @@ std::istream& binread(std::istream& in, T& t)
     return in.read(reinterpret_cast<char*>(&t), sizeof(T));
 }
 
+
+std::string buffread(std::istream& in, size_t bytesToRead)
+{
+    auto buffer = std::string(bytesToRead, '\0');
+    in.read(&buffer[0], bytesToRead);
+    return buffer;
+}
+
 template<typename T>
 std::ostream& binwrite(std::ostream& out, const T& t) {
     return out.write(reinterpret_cast<const char*>(&t), sizeof(T));
