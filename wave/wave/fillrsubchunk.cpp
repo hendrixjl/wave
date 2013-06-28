52922342d91f3f0264b17d5e989b83d6c9b95fc2
hendrixjl <hendrix.john64@gmail.com>
2013-06-28 13:33:05 -0500

    move code to .cpp

--- wave/wave/fillrsubchunk.cpp
+++ wave/wave/fillrsubchunk.cpp
@@ -18,3 +18,22 @@ fillrsubchunk::fillrsubchunk(std::istream& in) : SubchunkId("FLLR") {
     Data = std::string(SubchunkSize, ' ');
     in.read(&Data[0], SubchunkSize);
 }
+
+fillrsubchunk::fillrsubchunk(uint32_t size, const std::string& data)
+: SubchunkId("FLLR"), SubchunkSize(size), Data(data) {
+}
+    
+    
+std::ostream& fillrsubchunk::textout(std::ostream& out) const {
+    out << "SubchunkId=" << SubchunkId;
+    out << " SubchunkSize=" << SubchunkSize;
+    out << " {" << size() << "}";
+    return out;
+}
+    
+std::ostream& fillrsubchunk::binout(std::ostream& out) const {
+    out.write(SubchunkId.c_str(), SUBCHUNKID_SIZE);
+    binwrite(out, SubchunkSize);
+    out.write(Data.c_str(), Data.size());
+    return out;
+}
