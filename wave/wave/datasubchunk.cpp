cc8db4b19a7ef0dce875c74d3c6fa1e1606c1934
hendrixjl <hendrix.john64@gmail.com>
2013-06-29 09:23:17 -0500

    better procedural decomposition.

--- wave/wave/datasubchunk.cpp
+++ wave/wave/datasubchunk.cpp
@@ -15,28 +15,51 @@ using namespace std;
 
 static auto b = subchunk_factory::instance().register_type<datasubchunk>("data", create_subchunk<datasubchunk>());
 
-
+// Convert a block of bytes to 8-bit sample data
 uint8_t bytesToSample(const char *b, uint8_t& sample) {
     sample = static_cast<uint8_t>(*b);
     return sample;
 }
 
+// Convert a block of bytes to 16-bit sample data
 int16_t bytesToSample(const char *b, int16_t& sample) {
     sample = static_cast<int16_t>(uint16_t(b[0]) + uint16_t(b[1])*0x0100);
     return sample;
 }
 
+// Convert a block of bytes to 32-bit sample data
+int32_t bytesToSample(const char *b, int32_t& sample) {
+    sample = static_cast<int32_t>(uint32_t(b[0]) +
+                                  uint32_t(b[1])*0x0100 +
+                                  uint32_t(b[2])*0x010000 +
+                                  uint32_t(b[2])*0x01000000);
+    return sample;
+}
+
+// Convert a block of bytes to a vector of sample data
+template<typename T>
+vector<T> from_bytebuffer(const string& buffer, vector<T>& result) {
+    for (auto i=0; i<buffer.size()/sizeof(T); ++i) {
+        T sample{};
+        bytesToSample(&buffer[i*sizeof(T)], sample);
+        result.push_back( sample );
+    }
+    return result;
+}
+
+// Read a file stream into a vector of sample data
+template<typename T>
+vector<T> from_stream(istream& in, uint32_t bytes, vector<T>& result) {
+    auto data = buffered_read(in, bytes);
+    result = from_bytebuffer(data, result);
+    return result;
+}
 
 datasubchunk::datasubchunk(std::istream& in)
-: SubchunkId{"data"}
+: SubchunkId{"data"},
+SubchunkSize{binread(in,SubchunkSize)},
+Data{from_stream(in, SubchunkSize, Data)}
 {
-    binread(in, SubchunkSize);
-    auto data = buffered_read(in, SubchunkSize);
-    for (auto i=0; i<SubchunkSize/sizeof(Sample_t); ++i) {
-        Sample_t sample{};
-        bytesToSample(&data[i*sizeof(Sample_t)], sample);
-        Data.push_back( sample );
-    }
 }
 
 datasubchunk::datasubchunk(uint32_t size, const std::vector<Sample_t>& data)
