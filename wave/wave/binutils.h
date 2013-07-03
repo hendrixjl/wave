f0c23eb20edfee431a02b42d2a2ca336adf70a25
hendrixjl <hendrix.john64@gmail.com>
2013-07-02 20:31:47 -0500

    Moving logic from datasubchunk class files.

--- wave/wave/binutils.h
+++ wave/wave/binutils.h
@@ -10,6 +10,9 @@
 #define wave_binutils_h
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
 
 /**
  * @brief Read "raw" bytes from an istream directly into
@@ -61,6 +64,52 @@ std::ostream& binwrite(std::ostream& out, const T& t) {
  */
 inline std::string FormattedDataSubchunkId(uint16_t numChannels, uint16_t bitsPerSample) {
     return std::string{"data_"} + std::to_string(numChannels) + "_" + std::to_string(bitsPerSample);
+}
+
+// Convert a block of bytes to 8-bit sample data
+inline uint8_t bytesToSample(const char *b, uint8_t& sample) {
+    sample = static_cast<uint8_t>(*b);
+    return sample;
+}
+
+// Convert a block of bytes to 16-bit sample data
+inline int16_t bytesToSample(const char *b, int16_t& sample) {
+    sample = static_cast<int16_t>(uint16_t(b[0]) + uint16_t(b[1])*0x0100);
+    return sample;
+}
+
+// Convert a block of bytes to 32-bit sample data
+inline int32_t bytesToSample(const char *b, int32_t& sample) {
+    sample = static_cast<int32_t>(uint32_t(b[0]) +
+                                  uint32_t(b[1])*0x0100 +
+                                  uint32_t(b[2])*0x010000 +
+                                  uint32_t(b[2])*0x01000000);
+    return sample;
+}
+
+// Convert a block of bytes to a vector of sample data
+template<typename T>
+std::vector<T> from_bytebuffer(const std::string& buffer, std::vector<T>& result) {
+    for (auto i=0; i<buffer.size()/sizeof(T); ++i) {
+        T sample{};
+        bytesToSample(&buffer[i*sizeof(T)], sample);
+        result.push_back( sample );
+    }
+    return result;
+}
+
+// Read a file stream into a vector of sample data
+//    Assumption: bytes is a whole multiple of sizeof(T)
+template<typename T>
+std::vector<T> from_stream(std::istream& in, uint32_t bytes, std::vector<T>& result) {
+    if ((bytes % sizeof(T)) != 0) {
+        throw std::length_error(std::string(__func__) +
+                                ": Data size is not a multiple of sample type " +
+                                typeid(T).name() + " size!");
+    }
+    auto data = buffered_read(in, bytes);
+    result = from_bytebuffer(data, result);
+    return result;
 }
 
 #endif
